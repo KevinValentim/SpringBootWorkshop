472699aa6262780a0e074fce26559ef9592cf80e
Kevin Valentim <KevinValentim61@gmail.com>
2024-05-06 16:32:49 -0300

    H2 database, test profile, JPA

--- src/main/java/com/kevinDev/course/resources/UserResource.java
+++ src/main/java/com/kevinDev/course/resources/UserResource.java
@@ -1,5 +1,21 @@
 package com.kevinDev.course.resources;
 
-public class UserResource {
+import org.springframework.web.bind.annotation.RestController;
 
+import com.kevinDev.course.entities.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, null, null, null, null);
+		return ResponseEntity.ok().body(u);
+	}
 }
